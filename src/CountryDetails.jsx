ced35bdbd403d4106852b345b51cc17c20d2a264
thekingvice <villarrealvicente92@gmail.com>
2023-04-25 16:42:26 -0700

    filter by region added

--- src/CountryDetails.jsx
+++ src/CountryDetails.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import Bordering from "./Bordering";
 
 export default function CountryDetails() {
   const { id } = useParams();
--- src/CountryList.jsx
+++ src/CountryList.jsx
@@ -7,28 +7,42 @@ export default function CountryList() {
   const [countries, setCountries] = useState([]);
   const [filteredCountries, setFilteredCountries] = useState([]);
   const [searchQuery, setSearchQuery] = useState("");
+  const [regions, setRegions] = useState([
+    "Africa",
+    "America",
+    "Asia",
+    "Europe",
+    "Oceania",
+  ]);
   useEffect(() => {
     fetch("https://restcountries.com/v3.1/all")
       .then((res) => res.json())
       .then((data) => {
         setCountries(data);
       });
-  });
+  }, []);
 
   useEffect(() => {
     if (filteredCountries.length === 0) {
       setFilteredCountries(countries);
     }
-  });
+  }, [filteredCountries, countries]);
   const handleSearchChange = (event) => {
     const query = event.target.value;
     setSearchQuery(query);
-    console.log(query);
+    // console.log(query);
     const filtered = countries.filter((country) =>
       country.name.common.toLowerCase().startsWith(query.toLowerCase())
     );
-    console.log(filtered);
+    // console.log(filtered);
     setFilteredCountries(filtered);
+  };
+
+  const handleRegionChange = (region) => {
+    const countriesByRegion = countries.filter(
+      (country) => country.region === region
+    );
+    setFilteredCountries(countriesByRegion);
   };
 
   return (
@@ -40,6 +54,47 @@ export default function CountryList() {
       >
         test
       </button>
+
+      <div>
+        <div>Filter By Region</div>
+        <div>
+          <p
+            onClick={() => {
+              handleRegionChange(regions[0]);
+            }}
+          >
+            {regions[0]}
+          </p>
+          <p
+            onClick={() => {
+              handleRegionChange(regions[1]);
+            }}
+          >
+            {regions[1]}
+          </p>
+          <p
+            onClick={() => {
+              handleRegionChange(regions[2]);
+            }}
+          >
+            {regions[2]}
+          </p>
+          <p
+            onClick={() => {
+              handleRegionChange(regions[3]);
+            }}
+          >
+            {regions[3]}
+          </p>
+          <p
+            onClick={() => {
+              handleRegionChange(regions[4]);
+            }}
+          >
+            {regions[4]}
+          </p>
+        </div>
+      </div>
       <input type="text" value={searchQuery} onChange={handleSearchChange} />
       {filteredCountries.map((country) => (
         <Link to={`/${country.cca3}`} key={country.cca3}>
