e7572f20bcbae53fb19fa564f41628f82de07d1c
thekingvice <villarrealvicente92@gmail.com>
2023-04-30 21:47:04 -0700

    commit

--- src/CountryDetails.jsx
+++ src/CountryDetails.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import "./CountryDetails.css";
 
 export default function CountryDetails() {
   const { id } = useParams();
   const [country, setCountry] = useState({});
   const [countryName, setCountryName] = useState("");
+  const [flag, setFlag] = useState("");
+  const [nativeName, setNativeName] = useState("");
+  const [population, setPopulation] = useState("");
+  const [region, setRegion] = useState("");
+  const [subregion, setSubregion] = useState("");
+  const [capital, setCapital] = useState("");
 
   useEffect(() => {
     fetch(`https://restcountries.com/v3.1/alpha/${id}`)
@@ -18,6 +25,12 @@ export default function CountryDetails() {
   useEffect(() => {
     if (country && country[0]) {
       setCountryName(country[0].name.common);
+      setFlag(country[0].flags.svg);
+      setNativeName(country[0].name.official);
+      setPopulation(country[0].population);
+      setRegion(country[0].region);
+      setSubregion(country[0].subregion);
+      setCapital(country[0].capital);
     }
   }, [country]);
 
@@ -27,6 +40,37 @@ export default function CountryDetails() {
 
   return (
     <>
+      <main className="CountryDetails">
+        <img className="CountryDetails__flag" src={flag} alt="" />
+        <div>
+          <h1>{countryName}</h1>
+          <p>
+            <b>Native Name:</b> {nativeName}
+          </p>
+          <p>
+            <b>Population: </b>
+            {population.toLocaleString(undefined, {
+              useGrouping: true,
+              maximumFractionDigits: 0,
+            })}
+          </p>
+          <p>
+            <b>Region:</b> {region}
+          </p>
+          <p>
+            <b>Sub Region:</b> {subregion}
+          </p>
+          <p>
+            <b>Capital:</b> {capital}
+          </p>
+          <p>
+            <b>Currencies:</b>
+          </p>
+          <p>
+            <b>Languages:</b>
+          </p>
+        </div>
+      </main>
       <div onClick={handleClick}>Country Name:{countryName}</div>
     </>
   );
