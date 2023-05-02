f0e762044331a3a2a0cdd3200a652975de63d5bc
thekingvice <villarrealvicente92@gmail.com>
2023-05-01 22:05:19 -0700

    commit

--- src/CountryDetails.jsx
+++ src/CountryDetails.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import "./CountryDetails.css";
@@ -13,6 +14,8 @@ export default function CountryDetails() {
   const [region, setRegion] = useState("");
   const [subregion, setSubregion] = useState("");
   const [capital, setCapital] = useState("");
+  const [languages, setLanguages] = useState("");
+  const [currencies, setCurrencies] = useState("");
 
   useEffect(() => {
     fetch(`https://restcountries.com/v3.1/alpha/${id}`)
@@ -31,6 +34,12 @@ export default function CountryDetails() {
       setRegion(country[0].region);
       setSubregion(country[0].subregion);
       setCapital(country[0].capital);
+      setCurrencies(
+        Object.values(country[0].currencies)
+          .map((o) => o.name)
+          .join(", ")
+      );
+      setLanguages(Object.values(country[0].languages).join(", "));
     }
   }, [country]);
 
@@ -41,34 +50,48 @@ export default function CountryDetails() {
   return (
     <>
       <main className="CountryDetails">
-        <img className="CountryDetails__flag" src={flag} alt="" />
-        <div>
-          <h1>{countryName}</h1>
-          <p>
-            <b>Native Name:</b> {nativeName}
-          </p>
-          <p>
-            <b>Population: </b>
-            {population.toLocaleString(undefined, {
-              useGrouping: true,
-              maximumFractionDigits: 0,
-            })}
-          </p>
-          <p>
-            <b>Region:</b> {region}
-          </p>
-          <p>
-            <b>Sub Region:</b> {subregion}
-          </p>
-          <p>
-            <b>Capital:</b> {capital}
-          </p>
-          <p>
-            <b>Currencies:</b>
-          </p>
-          <p>
-            <b>Languages:</b>
-          </p>
+        <div className="CountryDetails__wrapper">
+          <div className="CountryDetails__flag-and-back-wrapper">
+            <Link className="CountryDetails__back" to={"/"}>
+              <img
+                className="CountryDetails__back-arrow"
+                src="./arrow_back.svg"
+                alt="back-arrow"
+              />
+              Back
+            </Link>
+            <img className="CountryDetails__flag" src={flag} alt="" />
+          </div>
+
+          <div className="CountryDetails__info">
+            <h1 className="CountryDetails__header">{countryName}</h1>
+
+            <p>
+              <b>Native Name:</b> {nativeName}
+            </p>
+            <p>
+              <b>Population: </b>
+              {population.toLocaleString(undefined, {
+                useGrouping: true,
+                maximumFractionDigits: 0,
+              })}
+            </p>
+            <p>
+              <b>Region:</b> {region}
+            </p>
+            <p>
+              <b>Sub Region:</b> {subregion}
+            </p>
+            <p>
+              <b>Capital:</b> {capital}
+            </p>
+            <p>
+              <b>Currencies:</b> {currencies}
+            </p>
+            <p>
+              <b>Languages:</b> {languages}
+            </p>
+          </div>
         </div>
       </main>
       <div onClick={handleClick}>Country Name:{countryName}</div>
