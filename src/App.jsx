241a51f86c13d142f5a93fbc7110ee1d17453738
thekingvice <villarrealvicente92@gmail.com>
2023-05-03 21:47:47 -0700

    pass down props for setdarkmode

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,36 @@ import "./App.css";
 import CountryDetails from "./CountryDetails";
 
 function App() {
+  const [styleColors, setStyleColors] = useState({
+    dark_grey: "#202d36",
+    grey: "2b3743",
+    white: "white",
+    filter_white: `brightness(0) saturate(100%) invert(100%) sepia(11%)
+    saturate(7487%) hue-rotate(169deg) brightness(120%) contrast(101%)`,
+  });
+
+  const UnsetDarkMode = {
+    dark_grey: "unset",
+    grey: "unset",
+    white: "unset",
+    filter_white: "unset",
+  };
+
+  const DarkMode = {
+    dark_grey: "#202d36",
+    grey: "2b3743",
+    white: "white",
+    filter_white: `brightness(0) saturate(100%) invert(100%) sepia(11%)
+    saturate(7487%) hue-rotate(169deg) brightness(120%) contrast(101%)`,
+  };
+
   return (
-    <Routes>
-      <Route path="/" element={<CountryList />}></Route>
-      <Route path="/:id" element={<CountryDetails />}></Route>
-    </Routes>
+    <div>
+      <Routes>
+        <Route path="/" element={<CountryList />}></Route>
+        <Route path="/:id" element={<CountryDetails />}></Route>
+      </Routes>
+    </div>
   );
 }
 
